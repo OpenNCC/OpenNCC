ce8238b9ade41a55a1028f37febaa53cf1151078
M4tsuri <me@m4tsuri.com>
2023-07-22 13:32:51 +0800

    Fixed broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/前言',
               },
             ],
           },
@@ -108,10 +108,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
